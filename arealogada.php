a112e1e84a6af2c2f67ec0d28da7787fe3f8b291
winissius <winissius.m@gmail.com>
2022-11-23 07:32:37 -0300

    Adição dos itens emprestados

--- arealogada.php
+++ arealogada.php
@@ -25,6 +25,38 @@
    </header>
    <h2>Meus items emprestados</h2> 
    <hr size="1" width="100%">
+        <?php
+            $query = "SELECT descricao, tipo, quantidade, disponibilidade, devolucao, tomador FROM itens WHERE dono='$user'";
+            $resultado = mysqli_query($conexao, $query);
+            $linhas = mysqli_fetch_assoc($resultado);
+            ?>
+            <div id='login' style="width: 1000px;">
+                <form action=""></form>
+                                <table border="1px">
+                                    <tr >
+                                        <td colspan="3">DESCRIÇÃO</td><td style="text-align: center;">TIPO</td><td colspan="3">QUANTIDADE</td><td colspan="3">DISPONIBILIDADE</td><td colspan="3">DEVOLUÇÃO</td><td>EMPRESTADOR</td>
+                                    </tr>
+            <?php
+            if((count($linhas))>0){
+                do {
+                    ?>
+                    <tr>
+                        <td colspan="3"><?=$linhas['descricao']?></td>
+                        <td style="text-align: center;"><?=$linhas['tipo']?></td>
+                        <td style="text-align: center;" colspan="3"><?=$linhas['quantidade']?></td>
+                        <td style="text-align: center;" colspan="3"><?=$linhas['disponibilidade']?></td>
+                        <td style="text-align: center;" colspan="3"><?=$linhas['devolucao']?></td>
+                        <td style="text-align: center;"><?=$linhas['tomador']?></td>
+                    </tr>
+                                
+                                  
+                    <?php
+                            }while($linhas = mysqli_fetch_assoc($resultado));
+                        }
+                    ?>
+                    </table>       
+            </div>
+   <hr size="1" width="100%">
    <h2>Cadastrar novo item</h2>
    <hr size="1" width="100%">
     <div id="cadastroItens">
